9aba5c79252f29e09602aa14e885ecae84280a07
Alex Hughes <hughes.13.alex@gmail.com>
2019-02-26 17:29:30 -0800

    First Commit

---
+++ cmake-build-debug/CMakeFiles/Assignment5_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Assignment5_1.dir/main.cpp.o"
+  "Assignment5_1.pdb"
+  "Assignment5_1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Assignment5_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
